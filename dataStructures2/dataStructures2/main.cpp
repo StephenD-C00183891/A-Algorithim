29608d4482090c47ba41e58a4c5d9ef1fb20544e
StephenD-C00183891 <sdonegan4@gmail.com>
2016-02-15 14:20:27 +0000

    drawing graph

--- dataStructures2/dataStructures2/main.cpp
+++ dataStructures2/dataStructures2/main.cpp
@@ -38,10 +38,19 @@ void visit(Node * pNode) {
 }
 
 int main(int argc, char *argv[]) {
-   
-    Graph<tuple<string, int, int, int, int>,int>graph( 30 );
-	
+	// Create the main window 
+	sf::RenderWindow window(sf::VideoMode(800, 600, 32), "SFML First Program");
 
+	//load a font
+	sf::Font font;
+	font.loadFromFile("C:\\Windows\\Fonts\\GARA.TTF");
+
+	window.clear();
+
+    Graph<tuple<string, int, int, int, int>,int>graph( 30 );
+	vector<sf::CircleShape> circles;
+	vector<sf::VertexArray> lines2;
+	
 	string c;
 	int i = 0;
 	int x;
@@ -51,14 +60,24 @@ int main(int argc, char *argv[]) {
 	myfile.open ("nodes2.txt");
 	while ( myfile >> c >> x >> y) {
 		graph.addNode(tuple<string, int, int, int, int>(c, 99999999, 99999999, x, y), i++);
+
 	}
 	myfile.close();
 
 
 	myfile.open("arcs2.txt");
 	int from, to, weight;
-	while ( myfile >> from >> to >> weight ) {
+	while (myfile >> from >> to >> weight) {
 		graph.addArc(from, to, weight);
+
+		sf::VertexArray lines(sf::LinesStrip, 2);
+		lines[0].position = sf::Vector2f(get<3>(graph.nodeArray()[from]->data()) + 20, get<4>(graph.nodeArray()[from]->data()) + 20);
+		lines[0].color = sf::Color::Red;
+		lines[1].position = sf::Vector2f(get<3>(graph.nodeArray()[to]->data()) + 20, get<4>(graph.nodeArray()[to]->data()) + 20);
+		lines[1].color = sf::Color::Red;
+
+		lines2.push_back(lines);
+		//window.draw(lines);
 	}
     myfile.close();
 
@@ -74,5 +93,50 @@ int main(int argc, char *argv[]) {
 		visit(n);
 
 
-	system("PAUSE");
+	for (int i = 0; i < 30; i++) {
+		sf::CircleShape circle(20);
+		circle.setPosition(get<3>(graph.nodeArray()[i]->data()), get<4>(graph.nodeArray()[i]->data()));
+		circles.push_back(circle);
+
+	}
+
+	// Start game loop 
+	while (window.isOpen())
+	{
+		// Process events 
+		sf::Event Event;
+		while (window.pollEvent(Event))
+		{
+			// Close window : exit 
+			if (Event.type == sf::Event::Closed)
+				window.close();
+
+			// Escape key : exit 
+			if ((Event.type == sf::Event::KeyPressed) && (Event.key.code == sf::Keyboard::Escape))
+				window.close();
+		}
+
+		//window.clear();
+
+		for (int i = 0; i < 30; i++) {
+
+			window.draw(circles[i]);
+			
+		}
+		for (int i = 0; i < 74; i++)
+		{
+			window.draw(lines2[i]);
+		}
+
+		for (int i = 0; i < 30; i++) {
+
+			window.draw(circles[i]);
+		}
+		
+
+		window.display();
+	}
+
+	return EXIT_SUCCESS;
 }
+
